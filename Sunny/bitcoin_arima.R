c71d7a204ec23ae3e76a5f61109bdaf1bbaa8d03
sunnywong25 <sunnywong081018@gmail.com>
2024-04-07 02:41:44 +0800

    Update bitcoin_arima.R

--- Sunny/bitcoin_arima.R
+++ Sunny/bitcoin_arima.R
@@ -8,6 +8,7 @@ rmse <- function(y_true, y_pred){
   mse = mean((y_true - y_pred)^2)
   return(sqrt(mse))
 }
+
 # Load data
 btc = get.hist.quote(instrument = "BTC-USD", start = "2018-01-01", end = "2023-12-31", 
                      quote = c("Open", "High", "Low", "Close", "Volume"))
@@ -20,14 +21,14 @@ train = btc[index(btc) <= train_last_day]$Close
 test = btc[index(btc) > train_last_day]$Close
 
 # Tune ARIMA order (p,d,q) and train best ARIMA model
-(best_model<-auto.arima(train,max.p=5,max.q=5,max.d=5,seasonal=FALSE,ic='aic',allowdrift=FALSE))
+(best_model<-auto.arima(train,max.p=5,max.q=5,max.d=5,seasonal=FALSE,ic='bic',allowdrift=FALSE))
 
 # Ljung-Box test - Desired is HO not rejected, i.e. p > 0.05
 orders = arimaorder(best_model)
 Box.test(best_model$residuals, lag = 10, type = "Ljung-Box", fitdf = orders[1] + orders[3])
 orders
 
-# Evaluation
+# Prediction and Evaluation
 hist_data <- tail(train,30)
 arima_pred <- array(NA,dim=length(test))
 for(i in 1:length(test)){
@@ -36,15 +37,15 @@ for(i in 1:length(test)){
   arima_pred[i] <- predict(best_model,n.ahead=1)$pred[1]
   hist_data <- c(hist_data,test[i])
 }
-sqrt(mean((arima_pred - test)^2))
 
 # Plot of prediction
 plot(as.numeric(test), type = "l", ylab = "Price")
 lines(arima_pred, col = "red")
 
 # Closer look of the plot (performance is actually bad) 
-plot(as.numeric(test)[1:10], type = "l", ylab = "Price")
-lines(arima_pred, col = "red")
+#plot(as.numeric(test)[1:10], type = "l", ylab = "Price")
+#lines(arima_pred, col = "red")
 
 rmse(test, c(tail(train,1), test[-length(test)])) # naive prediction
-rmse(test, arima_pred) # arima prediction (very little improvement)
+rmse(test, arima_pred) # arima prediction (very little improvement or even worse)
+
