528e25791442eaa994e0be1780587ae52007e63e
sunnywong25 <sunnywong081018@gmail.com>
2024-04-16 04:09:49 +0800

    Update bitcoin_arima.R

--- Sunny/bitcoin_arima.R
+++ Sunny/bitcoin_arima.R
@@ -1,27 +1,29 @@
 library(tseries)
 library(quantmod)
 library(forecast)
-rmse <- function(y_true, y_pred){
+mse <- function(y_true, y_pred){
   y_true = as.numeric(y_true)
   y_pred = as.numeric(y_pred)
   mse = mean((y_true - y_pred)^2)
-  return(sqrt(mse))
+  return(mse)
 }
 
 # Load data
-btc = read.csv("btc_minute_20211001_to_20211231.csv")
-btc = btc[dim(btc)[1]:1, c("Date", "Close")]
-btc_time = strptime(btc$Date, format = "%Y-%m-%d %H:%M:%S")
+btc = get.hist.quote(instrument = "BTC-USD", "2018-01-01", "2023-12-31",
+                     quote = c("Close"),
+                     provider = c("yahoo"))
 
 plot(btc$Close, type = "l")
 
 # train-test split
-train_last_day = "2021-12-31"
-train = btc[btc_time < train_last_day,]$Close
-test = btc[btc_time >= train_last_day,]$Close
+train_last_day = "2022-12-31"
+train = btc[index(btc) <= train_last_day,]
+test = btc[index(btc) > train_last_day,]
 
 # Tune ARIMA order (p,d,q) and train best ARIMA model
-(best_model<-auto.arima(train,max.p=5,max.q=5,max.d=5,seasonal=FALSE,ic='bic',allowdrift=FALSE))
+(best_model<-auto.arima(train,max.p=5,max.q=5,max.d=5,seasonal=FALSE,ic='aic',allowdrift=FALSE))
+best_model = arima(train, order = c(2,1,5))
+best_model
 
 # Ljung-Box test - Desired is HO not rejected, i.e. p > 0.05
 orders = arimaorder(best_model)
@@ -31,9 +33,11 @@ orders
 # Prediction and Evaluation
 hist_data <- tail(train,30)
 arima_pred <- array(NA,dim=length(test))
+model_coef = best_model$coef
 for(i in 1:length(test)){
   #update the model with old estimates of ARIMA parameters
   best_model <- Arima(hist_data, model = best_model)
+  model_coef = c(model_coef, best_model$coef)
   arima_pred[i] <- predict(best_model,n.ahead=1)$pred[1]
   hist_data <- c(hist_data,test[i])
 }
@@ -46,5 +50,5 @@ lines(arima_pred, col = "red")
 plot(as.numeric(test)[1:50], type = "l", ylab = "Price")
 lines(arima_pred, col = "red")
 
-rmse(test, c(tail(train,1), test[-length(test)])) # naive prediction
-rmse(test, arima_pred) # arima prediction (very little improvement or even worse)
+mse(test, c(tail(train,1), test[-length(test)])) # naive prediction
+mse(test, arima_pred) # arima prediction (very little improvement or even worse)
